5d1a5ceb34881e1be3a00c9f9b38cb3b8582a338
김지우 <kimjiwoo@gimjiuui-MacBookPro.local>
2022-12-28 14:43:01 +0900

    #18/1228/modify/쿠버네티스 배포2

--- src/apis/Boards/board.service.ts
+++ src/apis/Boards/board.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class BoardService {
   aaa() {
-    return 'Hello World!';
+    return "Hello World!2";
   }
 }
